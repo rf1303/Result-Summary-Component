967d71dace9eee7c88a38f179c0b4cffd92b8591
Ramiro Fernandez <pulgaramiro@gmail.com>
2026-01-17 03:58:13 -0300

    your result

--- src/YourResult.jsx
+++ src/YourResult.jsx
@@ -7,17 +7,19 @@ export function YourResult() {
         ? 0
         : Math.round((jsonData.reduce((previous, current) => previous + current.score, 0)) / jsonData.length);
     return (
-        <div className='grid items-center justify-items-center gap-6'>
-            <h1 className='text-preset-4 text-navy-200 font-bold md:text-preset-3'>Your Result</h1>
-            <div className='grid items-center justify-items-center'>
-                <p className='text-preset-2 text-white font-extrabold md:text-preset-1'>{average}</p>
-                <p className='text-base text-navy-200 md:text-preset-4'>of 100</p>
-            </div>
-            <div className=''>
-                <p className=''>Great</p>
-                <p className=''>You scored higher than 65% of the people who have taken these tests.</p>
-            </div>
+        <div className="w-full h-clampResult-h grid items-center justify-items-center bg-linear-180 from-gradient-02  to-gradient-22 rounded-b-4xl">
+            <div className='w-65 grid items-center justify-items-center gap-6'>
+                <h1 className='text-preset-4 text-navy-200 font-bold md:text-preset-3'>Your Result</h1>
+                <div className='w-35 h-35 flex flex-col items-center justify-center rounded-full bg-linear-0 from-Persian-blue to-Violet-blue'>
+                    <p className='text-preset-2 text-white font-extrabold tracking-normal leading-none md:text-preset-1'>{average}</p>
+                    <p className='text-base text-navy-200 leading-none md:text-preset-4'>of 100</p>
+                </div>
+                <div className='grid items-center justify-items-center'>
+                    <p className='text-preset-3 text-white md:text-preset-5'>Great</p>
+                    <p className='text-base text-navy-200 md:text-preset-4'>You scored higher than 65% of the people who have taken these tests.</p>
+                </div>
 
+            </div>
         </div>
     );
 }
